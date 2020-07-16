b6525c7c8cf6afd6e1aa86232f2e3e3e38d19f3d
Frankie <zz1763@nyu.edu>
2020-07-16 15:01:08 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,35 +1,69 @@
-from glob import glob
+import os
 import tensorflow as tf
-from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
+import argparse
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping, LearningRateScheduler
 from deeplab import DeepLabV3Plus
 
 print('TensorFlow', tf.__version__)
 
+parser = argparse.ArgumentParser()
 
-batch_size = 24
+parser.add_argument('--txt_dir', type=str,
+                    default= '/content/drive/My Drive/CS Internship/DeepLab_v3/deeplab_v3_tensorflow_v1/dataset',
+                    help='directory that contains the train, val txt files.')
+parser.add_argument('--ckpt_dir', type=str,
+                    default= "/content/drive/My Drive/CS Internship/DeepLab_v3/deeplab_v3_plus_tensorflow_v2/checkpoints/training_1/cp-{epoch:04d}.ckpt",
+                    help='directory that saves checkpoints.')
+parser.add_argument('--tensorboard_dir', type=str,
+                    default= '/content/drive/My Drive/CS Internship/DeepLab_v3/deeplab_v3_plus_tensorflow_v2/logs',
+                    help='directory that saves tensorboard logs.')
+parser.add_argument('--restore', type=str,
+                    default= None,
+                    help='path of the checkpoint you want to restore.')
+parser.add_argument('--epoch', type=int,
+                    default= 300,
+                    help='number of epochs to train.')
+parser.add_argument('--saving_interval', type=int,
+                    default= 2,
+                    help='save every x epochs.')
+parser.add_argument('--m', type=int,
+                    default= 0.9997,
+                    help='training momentum.')
+parser.add_argument('--e', type=int,
+                    default= 1e-5,
+                    help='training epsilon.')
+parser.add_argument('--lr', type=int,
+                    default= 1e-4,
+                    help='learning rate.')
+
+# Global variables
+batch_size = 10
 H, W = 512, 512
-num_classes = 34
+num_classes = 22  # including background and the boundry pixels
+_DEPTH = 3
 
-image_list = sorted(glob(
-    'cityscapes/dataset/train_images/*'))
-mask_list = sorted(glob(
-    'cityscapes/dataset/train_masks/*'))
 
-val_image_list = sorted(glob(
-    'cityscapes/dataset/val_images/*'))
-val_mask_list = sorted(glob(
-    'cityscapes/dataset/val_masks/*'))
+def make_list_from_txt(txt_dir):
+    """"txt_dir: directory that contains the train, val txt files.
+        return: lists of file paths of train/val image data."""
+    f = open(txt_dir + 'train_img_full_path.txt', 'r')
+    train_img_list = [line[:-1] for line in f.readlines()]
+    f.close()
 
-print('Found', len(image_list), 'training images')
-print('Found', len(val_image_list), 'validation images')
+    f = open(txt_dir + 'train_msk_full_path.txt', 'r')
+    train_msk_list = [line[:-1] for line in f.readlines()]
+    f.close()
 
-for i in range(len(image_list)):
-    assert image_list[i].split(
-        '/')[-1].split('_leftImg8bit')[0] == mask_list[i].split('/')[-1].split('_gtFine_labelIds')[0]
+    f = open(txt_dir + 'val_img_full_path.txt', 'r')
+    val_img_list = [line[:-1] for line in f.readlines()]
+    f.close()
 
-for i in range(len(val_image_list)):
-    assert val_image_list[i].split('/')[-1].split('_leftImg8bit')[
-        0] == val_mask_list[i].split('/')[-1].split('_gtFine_labelIds')[0]
+    f = open(txt_dir + 'val_msk_full_path.txt', 'r')
+    val_msk_list = [line[:-1] for line in f.readlines()]
+    f.close()
+
+    assert len(train_img_list) == len(train_msk_list) and len(val_img_list) == len(val_msk_list)
+    return train_img_list, train_msk_list, val_img_list, val_msk_list
 
 
 def get_image(image_path, img_height=800, img_width=1600, mask=False, flip=0):
@@ -49,7 +83,7 @@ def get_image(image_path, img_height=800, img_width=1600, mask=False, flip=0):
     else:
         img = tf.image.decode_png(img, channels=1)
         img = tf.cast(tf.image.resize(images=img, size=[
-                      img_height, img_width]), dtype=tf.uint8)
+            img_height, img_width]), dtype=tf.uint8)
         img = tf.case([
             (tf.greater(flip, 0), lambda: tf.image.flip_left_right(img))
         ], default=lambda: img)
@@ -85,55 +119,78 @@ def load_data(image_path, mask_path, H=512, W=512):
     return image, mask
 
 
-train_dataset = tf.data.Dataset.from_tensor_slices((image_list,
-                                                    mask_list))
-train_dataset = train_dataset.shuffle(buffer_size=128)
-train_dataset = train_dataset.apply(
-    tf.data.experimental.map_and_batch(map_func=load_data,
-                                       batch_size=batch_size,
-                                       num_parallel_calls=tf.data.experimental.AUTOTUNE,
-                                       drop_remainder=True))
-train_dataset = train_dataset.repeat()
-train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
-print(train_dataset)
+def make_dataset(train_img_list, train_msk_list, val_img_list, val_msk_list):
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_img_list, train_msk_list))
+    train_dataset = train_dataset.shuffle(buffer_size=128)
+    train_dataset = train_dataset.apply(
+        tf.data.experimental.map_and_batch(map_func=load_data,
+                                           batch_size=batch_size,
+                                           num_parallel_calls=tf.data.experimental.AUTOTUNE,
+                                           drop_remainder=True))
+    train_dataset = train_dataset.repeat()
+    train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
-val_dataset = tf.data.Dataset.from_tensor_slices((val_image_list,
-                                                  val_mask_list))
-val_dataset = val_dataset.apply(
-    tf.data.experimental.map_and_batch(map_func=load_data,
-                                       batch_size=batch_size,
-                                       num_parallel_calls=tf.data.experimental.AUTOTUNE,
-                                       drop_remainder=True))
-val_dataset = val_dataset.repeat()
-val_dataset = val_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    val_dataset = tf.data.Dataset.from_tensor_slices((val_img_list,
+                                                      val_msk_list))
+    val_dataset = val_dataset.apply(
+        tf.data.experimental.map_and_batch(map_func=load_data,
+                                           batch_size=batch_size,
+                                           num_parallel_calls=tf.data.experimental.AUTOTUNE,
+                                           drop_remainder=True))
+    val_dataset = val_dataset.repeat()
+    val_dataset = val_dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+    return train_dataset, val_dataset
+
+def define_model(H, W, num_classes, momentum = 0.9997, epsilon = 1e-5, learning_rate = 1e-4)
+    loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
+    strategy = tf.distribute.MirroredStrategy()
+    with strategy.scope():
+        model = DeepLabV3Plus(H, W, num_classes)
+        for layer in model.layers:
+            if isinstance(layer, tf.keras.layers.BatchNormalization):
+                layer.momentum = momentum
+                layer.epsilon = epsilon
+            elif isinstance(layer, tf.keras.layers.Conv2D):
+                layer.kernel_regularizer = tf.keras.regularizers.l2(1e-4)
+        model.compile(loss=loss,
+                      optimizer=tf.optimizers.Adam(learning_rate=learning_rate),
+                      metrics=['accuracy'])
+    return model
+
+def define_callbacks(tb_logs_path, checkpoint_path, saving_interval = 2):
+    tb = TensorBoard(log_dir=tb_logs_path, write_graph=True, update_freq='batch')
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    mc = ModelCheckpoint(mode='min',
+                         filepath=checkpoint_path,
+                         monitor='val_loss',
+                         save_weights_only='True',
+                         period=saving_interval,  # save the checkpoint every x epochs
+                         verbose=1)
+    return [mc, tb]
+
+def main():
+    FLAGS, unparsed = parser.parse_known_args()
+    train_img_list, train_msk_list, val_img_list, val_msk_list = make_list_from_txt(FLAGS.txt_dir)
+    train_dataset, val_dataset = make_dataset(train_img_list, train_msk_list, val_img_list, val_msk_list)
+    momentum, epsilon, learning_rate = FLAGS.m, FLAGS.e, FLAGS.lr
+    model = define_model(H, W, num_classes, momentum, epsilon, learning_rate)
+    callbacks = define_callbacks(FLAGS.tensorboad_dir, FLAGS.checkpoint_dir, FLAGS.saving_interval)
+    if FLAGS.restore: # the restore flag is not None
+        strategy = tf.distribute.MirroredStrategy()
+        with strategy.scope():
+            model.load_weight(FLAGS.restore)
+    model.fit(train_dataset,
+              steps_per_epoch=len(train_img_list) // batch_size,
+              epochs=FLAGS.epoch,
+              verbose=1,
+              validation_data=val_dataset,
+              validation_steps=len(val_img_list) // batch_size,
+              callbacks=callbacks)
 
 
-loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
-strategy = tf.distribute.MirroredStrategy()
-with strategy.scope():
-    model = DeepLabV3Plus(H, W, num_classes)
-    for layer in model.layers:
-        if isinstance(layer, tf.keras.layers.BatchNormalization):
-            layer.momentum = 0.9997
-            layer.epsilon = 1e-5
-        elif isinstance(layer, tf.keras.layers.Conv2D):
-            layer.kernel_regularizer = tf.keras.regularizers.l2(1e-4)
-    model.compile(loss=loss, 
-                  optimizer=tf.optimizers.Adam(learning_rate=1e-4), 
-                  metrics=['accuracy'])
 
 
-tb = TensorBoard(log_dir='logs', write_graph=True, update_freq='batch')
-mc = ModelCheckpoint(mode='min', filepath='top_weights.h5',
-                     monitor='val_loss',
-                     save_best_only='True',
-                     save_weights_only='True', verbose=1)
-callbacks = [mc, tb]
 
 
-model.fit(train_dataset,
-          steps_per_epoch=len(image_list) // batch_size,
-          epochs=300,
-          validation_data=val_dataset,
-          validation_steps=len(val_image_list) // batch_size,
-          callbacks=callbacks)
+
