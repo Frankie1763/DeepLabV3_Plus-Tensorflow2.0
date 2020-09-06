0fb78a00d871f79e04e9f7d97f966ecd205ee310
Frankie <zz1763@nyu.edu>
2020-09-06 16:22:05 +0800

    Update train.py

--- train.py
+++ train.py
@@ -206,8 +206,8 @@ class MyWeightedLoss(tf.keras.losses.SparseCategoricalCrossentropy):
 
     def call(self, y_true, y_pred):
         sample_weight = np.zeros((y_pred.shape[0], y_pred.shape[1]))
-        for i in range(sample_weight):
-            for j in range(sample_weight[0]):
+        for i in range(y_pred.shape[0]):
+            for j in range(y_pred.shape[1]):
                 if y_pred[i][j] != 21:
                     sample_weight[i][j] = 1
         graph_ctx = tf_utils.graph_context_for_symbolic_tensors(
